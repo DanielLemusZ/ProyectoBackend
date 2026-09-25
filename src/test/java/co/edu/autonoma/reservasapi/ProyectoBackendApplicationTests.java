cc46cf1245262de577acc0b7b35235cad386cffa
Daniel Lemus <daniel_lemus_z@hotmail.com>
2026-09-24 19:44:29 -0500

    Commit inicial para sincronizar el repositorio

---
+++ src/test/java/co/edu/autonoma/reservasapi/ProyectoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.autonoma.reservasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
